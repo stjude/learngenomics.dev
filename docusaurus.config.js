d860b15a97f65cbe0a4a7d3d0bd3b2b8a76d8858
Jay Knight <jay.knight@stjude.org>
2022-11-14 14:01:39 -0600

    Fix 'Edit this page' links
    "Edit this page" links point to a https://github.com/facebook/docusaurus/ URL (which don't exist, of course). This makes those links point to this repo instead.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -26,13 +26,13 @@ const config = {
           sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+            "https://github.com/stjude/learngenomics.dev/tree/main/",
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+            "https://github.com/stjude/learngenomics.dev/tree/main/",
         },
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
